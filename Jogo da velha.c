ccef6c571c1a08514a3957b3888741b28fc50a47
Emylli Soares <100449171+emyllisoares@users.noreply.github.com>
2024-03-25 22:47:47 +0000

    com funções e procedimentos

--- Jogo da velha.c
+++ Jogo da velha.c
@@ -1,139 +1,129 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 #define PURPLE "\033[35m"
-#define RED "\033[31m"
+#define CYAN "\033[36m"
 #define FIM_COR "\033[0m"
+
+char jogo[3][3];
+
+//funcao para inicializar o jogo
+void inicializa_jogo(){
+    int i, j;
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++)
+            jogo[i][j] = ' ';
+    }
+}
+
+//funcao para imprimir o jogo
+void imprimir(){
+    int i, j;
+    printf(PURPLE"\n\n\t ====== Jogo da Velha ======\n" FIM_COR);
+    printf("\n\n\t 0   1   2\n\n");
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++){
+            if(j == 0)
+                printf("\t");
+            printf(" %c ", jogo[i][j]);
+            if(j < 2)
+                printf("|");
+            if(j == 2)
+                printf("  %d", i);
+        }
+        printf("\n");
+        if(i < 2)
+            printf("\t-----------\n");
+    }
+}
+
+//funcao para verificar vitoria por linha
+int vitoria_linha(char c){
+    int i;
+    for(i = 0; i < 3; i++){
+        if(jogo[i][0] == c && jogo[i][1] == c && jogo[i][2] == c)
+            return 1;
+    }
+    return 0;
+}
+
+//funcao para verificar vitoria por coluna
+int vitoria_coluna(char c){
+    int i;
+    for(i = 0; i < 3; i++){
+        if(jogo[0][i] == c && jogo[1][i] == c && jogo[2][i] == c)
+            return 1;
+    }
+    return 0;
+}
+
+//funcao para verificar vitoria por diagonal
+int vitoria_diagonal(char c){
+    if(jogo[0][0] == c && jogo[1][1] == c && jogo[2][2] == c)
+        return 1;
+    if(jogo[0][2] == c && jogo[1][1] == c && jogo[2][0] == c)
+        return 1;
+    return 0;
+}
+
+//funcao para verificar vitoria
+int vitoria(char c){
+    if(vitoria_linha(c) || vitoria_coluna(c) || vitoria_diagonal(c))
+        return 1;
+    return 0;
+}
+
+//funcao para verificar empate
+int empate(){
+    int i, j;
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++){
+            if(jogo[i][j] == ' ')
+                return 0;
+        }
+    }
+    return 1;
+}
+
+//funcao para jogar
+void jogar(){
+    int i;
+    int linha, coluna;
+    char c = 'X';
+    for(i = 0; i < 9; i++){
+        printf("\n\nJogador %d\n", i % 2 + 1);
+        printf("Digite a linha: ");
+        scanf("%d", &linha);
+        printf("Digite a coluna: ");
+        scanf("%d", &coluna);
+        if(jogo[linha][coluna] == ' '){
+            if(i % 2 == 0)
+                c = 'X';
+            else
+                c = 'O';
+            jogo[linha][coluna] = c;
+            imprimir();
+            if(vitoria(c)){
+                printf("\n\nJogador %d venceu!\n", i % 2 + 1);
+                break;
+            }
+            if(empate()){
+                printf(PURPLE"\n\nEmpate!\n"FIM_COR);
+                break;
+            }
+        }
+        else{
+            printf(CYAN"\n\nPosicao ocupada!\n"FIM_COR);
+            i--;
+        }
+    }
+}
+
 
 int main(){
 
-    // Estrutura de dados
-    int i, j, linha, coluna, jogador, ganhou, jogadas, opcao;
-    char jogo[3][3];
-
-    do{ // deseja jogar novamente?
-        jogador = 1;
-        ganhou = 0;
-        jogadas = 0;
-        // como inicializar a matriz?
-        for(i = 0; i < 3; i++){
-            for(j = 0; j < 3; j++){
-                jogo[i][j] = ' ';
-            }
-        }
-
-        do{ // repete até alguém ganhar ou atingir 9 jogadas
-            // imprimir jogo
-            printf("\n\n\t 0   1   2\n\n");
-            for(i = 0; i < 3; i++){
-                for(j = 0; j < 3; j++){
-                    if(j == 0)
-                        printf("\t");
-                    printf(" %c ", jogo[i][j]);
-                    if(j < 2)
-                        printf("|");
-                    if(j == 2)
-                        printf("  %d", i);
-                }
-                if(i < 2)
-                    printf("\n\t-----------");
-                printf("\n");
-            }
-
-            // ler coordenadas
-            do{
-                printf(PURPLE"\n\JOGADOR 1 = 0\nJOGADOR 2 = X\n"FIM_COR);
-                printf("\nJOGADOR %d: Digite a linha e a coluna que deseja jogar: ", jogador);
-                scanf("%d%d", &linha, &coluna);
-            }while(linha < 0 || linha > 2 || coluna < 0 || coluna > 2 || jogo[linha][coluna] != ' ');
-
-            // salvar coordenadas
-            if(jogador == 1){
-                jogo[linha][coluna] = '0';
-                jogador++;
-            }
-            else{
-                jogo[linha][coluna] = 'X';
-                jogador = 1;
-            }
-            jogadas++;
-
-            // alguém ganhou por linha
-            if(jogo[0][0] == '0' && jogo[0][1] == '0' && jogo[0][2] == '0' ||
-               jogo[1][0] == '0' && jogo[1][1] == '0' && jogo[1][2] == '0' ||
-               jogo[2][0] == '0' && jogo[2][1] == '0' && jogo[2][2] == '0'){
-                printf(PURPLE"\nO jogador 1 venceu!\n"FIM_COR);
-                ganhou = 1;
-            }
-
-            if(jogo[0][0] == 'X' && jogo[0][1] == 'X' && jogo[0][2] == 'X' ||
-               jogo[1][0] == 'X' && jogo[1][1] == 'X' && jogo[1][2] == 'X' ||
-               jogo[2][0] == 'X' && jogo[2][1] == 'X' && jogo[2][2] == 'X'){
-                printf(PURPLE"\nO jogador 2 venceu!\n"FIM_COR);
-                ganhou = 1;
-            }
-
-            // alguém ganhou por coluna
-            if(jogo[0][0] == '0' && jogo[1][0] == '0' && jogo[2][0] == '0' ||
-               jogo[0][1] == '0' && jogo[1][1] == '0' && jogo[2][1] == '0' ||
-               jogo[0][2] == '0' && jogo[1][2] == '0' && jogo[2][2] == '0'){
-                printf(PURPLE"\nO jogador 1 venceu!\n"FIM_COR);
-                ganhou = 1;
-            }
-
-            if(jogo[0][0] == 'X' && jogo[1][0] == 'X' && jogo[2][0] == 'X' ||
-               jogo[0][1] == 'X' && jogo[1][1] == 'X' && jogo[2][1] == 'X' ||
-               jogo[0][2] == 'X' && jogo[1][2] == 'X' && jogo[2][2] == 'X'){
-                printf(PURPLE"\nO jogador 2 venceu!\n"FIM_COR);
-                ganhou = 1;
-            }
-
-            // alguém ganhou na diagonal principal
-            if(jogo[0][0] == '0' && jogo[1][1] == '0' && jogo[2][2] == '0'){
-                printf(PURPLE"\nO jogador 1 venceu!\n"FIM_COR);
-                ganhou = 1;
-            }
-
-            if(jogo[0][0] == 'X' && jogo[1][1] == 'X' && jogo[2][2] == 'X'){
-                printf(PURPLE"\nO jogador 2 venceu!\n"FIM_COR);
-                ganhou = 1;
-            }
-
-            // alguém ganhou na diagonal secundária
-            if(jogo[0][2] == '0' && jogo[1][1] == '0' && jogo[2][0] == '0'){
-                printf(PURPLE"\nO jogador 1 venceu!\n"FIM_COR);
-                ganhou = 1;
-            }
-
-            if(jogo[0][2] == 'X' && jogo[1][1] == 'X' && jogo[2][0] == 'X'){
-                printf(PURPLE"\nO jogador 2 venceu!\n"FIM_COR);
-                ganhou = 1;
-            }
-        }while(ganhou == 0 && jogadas < 9);
-
-        // imprimir jogo
-        printf("\n\n\t 0   1   2\n\n");
-        for(i = 0; i < 3; i++){
-            for(j = 0; j < 3; j++){
-                if(j == 0)
-                    printf("\t");
-                printf(" %c ", jogo[i][j]);
-                if(j < 2)
-                    printf("|");
-                if(j == 2)
-                    printf("  %d", i);
-            }
-            if(i < 2)
-                printf("\n\t-----------");
-            printf("\n");
-        }
-
-        if(ganhou == 0)
-            printf(RED"\nDeu Velha! O jogo finalizou sem ganhador!\n"FIM_COR);
-
-        printf(PURPLE"\nDigite 1 para jogar novamente: \n"FIM_COR);
-        scanf("%d", &opcao);
-    }while(opcao == 1);
+    inicializa_jogo();
+    imprimir();
+    jogar();
 
     return 0;
 }
